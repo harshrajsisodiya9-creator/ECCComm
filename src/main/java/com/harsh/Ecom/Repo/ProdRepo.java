0fe659347c7ec9494675a629418d21e4271e9a42
harshrajsisodiya9-creator <harshrajsisodiya9@gmail.com>
2025-12-12 21:10:42 +0530

    mf last commit didnt happended beacuse i was in service folder

--- src/main/java/com/harsh/Ecom/Repo/ProdRepo.java
+++ src/main/java/com/harsh/Ecom/Repo/ProdRepo.java
@@ -2,8 +2,6 @@ package com.harsh.Ecom.Repo;
 
 import com.harsh.Ecom.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProdRepo extends JpaRepository<Product, Integer> {
 }
