c29e52bd56dbb10e6c25c2ccef378f0c1b173fec
harshrajsisodiya9-creator <harshrajsisodiya9@gmail.com>
2025-12-27 16:20:51 +0530

    no changes for now just before this merged MySql and DTO code to with master

--- src/main/java/com/harsh/Ecom/Repo/ProdRepo.java
+++ src/main/java/com/harsh/Ecom/Repo/ProdRepo.java
@@ -2,6 +2,7 @@ package com.harsh.Ecom.Repo;
 
 import com.harsh.Ecom.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
